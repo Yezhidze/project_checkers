c36d5e628ed06115955b03b9d8cc49025f6e0177
andruxa2200-ctrl <andruxa2200@gmail.com>
2026-02-12 20:15:18 +0700

    Посмотреть

--- project_checkers/main_menu.h
+++ project_checkers/main_menu.h
@@ -9,33 +9,11 @@
 //using namespace std;
 void showAsciiArt();
 void menu();
-void checkers_pole();
 
-/* Меню последовательность.
-    1. Начать игру.
-    2. Настройки музыки вкл\выкл
-    3. Выход (при нажатии на выход должен выходить из игры)
-    
-    Подмененю для Начать игру:
-    Выберете режим игры
-    1. Одиночная игра.
-    2. Игрок с игроком.
-    3. Выход в главное меню.
-    
-    При нажатии на режим "Одиночный режим" должен отправлять в функцию с одиночной игрой 
-    (его пока что нет, по этому напиши код без функции и закоментируй).
-
-    При нажатии на режим "Игрок с игроком" должен отправлять в фунцию checkers_pole().
-
-    При нажатии на "Выход в главное меню" должен отправлять в главное меню.
-    
-    ТАК ЖЕ ВАЖНО СОБЛЮДАТЬ ПРАВИЛА УСЛОВИЙ,
-    ЕСЛИ ПОЛЬЗОВАТЕЛЬ НАЖАЛ НЕВЕРНУЮ ЦИФРУ
-    ВЫВОДИТЬ ЧТО-ТО ТИПА "НЕВЕРНЫЙ ВЫБОР" И ПОВТОРНО 
-    ОТПРАВЛЯТЬ В МЕНЮ НА КОТОРОМ ОН ОШИБСЯ!!!!!!!!!! (ТЕКУЩЕЕ МЕНЮ)*/
-
+//#include "pole_checkers.h"
+//void checkers_pole();
 void showAsciiArt() {
-    // Установка локали для широких символов
+    
     setlocale(LC_ALL, "ru_RU.UTF-8");
     wcout.imbue(locale("ru_RU.UTF-8"));
 
@@ -67,147 +45,101 @@ void showAsciiArt() {
     {
         wcout << L"\t\t\t" << art_lines[i] << endl;
     }
-   wcout << L"\n\n";
+    wcout << L"\n\n";
     setlocale(LC_ALL, "ru");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     cout << "\t\t\t\t\tНажмите Enter для продолжения...";
     cin.get();
     system("cls");
-    menu();
+    /*   menu();*/
 }
 
 void menu()
 {
-    
+    setlocale(LC_ALL, "ru");
     int choice = 0;
     int checker_type = 1;
     int checker_color = 0;
     int player = 1;
-
+    showAsciiArt();
     while (true)
     {
-        std::cout << "\n ========\t ОСНОВНОЕ МЕНЮ \t    ======== \n " << endl;
-        std::cout << "  \t \t 0. Показать текущие настройки \t" << endl;
-        std::cout << "  \t \t 1. Выбрать игрока" << endl;
-        std::cout << "  \t \t 2. Выбрать цвет шашек" << endl;
-        std::cout << "  \t \t 3. Начать игру" << endl;
-        std::cout << "  \t \t 4. Выход" << endl;
-        std::cout << "  \t \t    Ваш выбор: \n \n ";
+        std::cout << "\t \t ------------------------------ \t" << endl;
+        std::cout << "\t \t|        ОСНОВНОЕ МЕНЮ         |\t" << endl;
+        std::cout << "\t \t ------------------------------ \t" << endl;
+        std::cout << "\t \t ------------------------------ \t" << endl;
+        std::cout << "\t \t|        1. Начать игру        |\t" << endl;
+        std::cout << "\t \t ------------------------------ \t" << endl;
+        std::cout << "\t \t ------------------------------ \t" << endl;
+        std::cout << "\t \t|        2.Настройки           |\t" << endl;
+        std::cout << "\t \t ------------------------------ \t" << endl;
+        std::cout << "\t \t ------------------------------ \t" << endl;
+        std::cout << "\t \t|        3.Выход               |\t" << endl;
+        std::cout << "\t \t ------------------------------ \t" << endl;
 
         if (player != 0 && checker_color != 0)
         {
-
-
             if (player == 1 && checker_color == 1)
             {
-                std::cout << "\t \t " << player << " Одиночная игра" << endl;
-                std::cout << "\t \t " << checker_color << " Вы вебрали белые шашки" << endl;
+                std::cout << " \t \t   - Одиночная игра" << std::endl;
+                std::cout << " \t \t   - Белые шашки" << std::endl;
+            }
+            else if (player == 1 && checker_color == 2)
+            {
+                std::cout << " \t \t   - Одиночная игра" << std::endl;
+                std::cout << " \t \t   - Черные шашки" << std::endl;
+            }
+            else if (player == 2 && checker_color == 1)
+            {
+                std::cout << " \t \t   - Игра на двоих" << std::endl;
+                std::cout << " \t \t   - Белые шашки" << std::endl;
             }
             else if (player == 2 && checker_color == 2)
             {
-                std::cout << "\t \t " << player << " Игра на двои" << endl;
-                std::cout << "\t \t " << checker_color << " Вы вебрали черные шашки" << endl;
+                std::cout << " \t \t   - Игра на двоих" << std::endl;
+                std::cout << " \t \t   - Черные шашки" << std::endl;
             }
             else
             {
-                std::cout << "\t \t Неправельный выбор " << endl;
+                std::cout << " \t \t   Неправильный выбор настроек" << std::endl;
             }
         }
+        std::cout << "    \t \t   Ваш выбор: ";
         std::cin >> choice;
         system("cls");
         switch (choice)
         {
         case 1:
-            std::cout << "\n \t ========\tМЕНЮ \t ======== \n " << endl;
-            std::cout << " \t \t  Выбор игрока  \n" << endl;
-            if (player == 1)
-            {
-                std::cout << " \t \t   Выбор игрока  " << endl;
-                std::cout << " \t \t 1 Одиночная игра " << endl;
-                std::cout << " \t \t 2 Игра на двоих \n" << endl;
-                std::cout << " \t \t   Ваш выбор: ";
-                std::cin >> player;
-
-                if (player == 1)
-                {
-                    std::cout << " \n \t \t  Одиночная игра  " << endl;
-                }
-                else if (player == 2)
-                {
-                    std::cout << " \n \t \t  Игра на двоих " << endl;
-                }
-                else
-                {
-                    std::cout << "Неверный выбор " << endl;
-                    player = 0;
-                }
-            }
-            std::cout << " \n \t \t   Нажмите Enter для продолжения...";
-            std::cin.ignore();
-            std::cin.get();
-            system("cls");
-            break;
-
-        case 2:
-
-            std::cout << "\n \t ========\tМЕНЮ \t ======== \n " << endl;
-            std::cout << " \t \t  Текущие настройки: \n" << endl;
-            std::cout << "  \t \t    Выбрать цвет шашек" << endl;
-            std::cout << "  \t \t    Ваш выбор: ";
-
-            if (checker_color == 0)
-            {
-                std::cout << " \t \t   Выберите цвет шашек:" << endl;
-                std::cout << " \t \t 1 Белые шашки" << endl;
-                std::cout << " \t \t 2 Черные шашки \n" << endl;
-                std::cout << " \t \t   Ваш выбор: ";
-                std::cin >> checker_color;
-
-                if (checker_color == 1)
-                {
-                    std::cout << " \n \t \t   Вы вебрали белые шашки " << endl;
-                }
-                else if (checker_color == 2)
-                {
-                    std::cout << " \n \t \t   Вы вебрали черные шашки " << endl;
-                }
-                else
-                {
-                    std::cout << "Неверный выбор " << endl;
-                    checker_color = 0;
-                }
-            }
-            std::cout << " \n \t \t   Нажмите Enter для возврата в меню...";
-            std::cin.ignore();
-            std::cin.get();
-            system("cls");
-            break;
-
-        case 3:
-            std::cout << "\n \t ========\tНАЧАЛО ИГРЫ \t ======== \n " << endl;
+            std::cout << "\t \t ------------------------------ \t" << endl;
+            std::cout << "\t \t| ======= НАЧАЛА ИГРЫ ======== |\t" << endl;
+            std::cout << "\t \t ------------------------------ \t" << endl;
 
             if (player == 0 || checker_color == 0)
             {
 
-                std::cout << " \t \t   Ошибка: не все настройки выбраны!" << std::endl;
+                std::cout << "  \t \t ------------------------------ \t" << endl;
+                std::cout << "  \t \t|   Не все настройки выбраны!  |\t" << endl;
+                std::cout << "  \t \t|   Необходимо выбрать:        |\t" << endl;
+                std::cout << "  \t \t ------------------------------ \t" << endl;
+                std::cout << "  \t \t ------------------------------ \t" << endl;
+                std::cout << "  \t \t| Вернитесь в меню для выбора. |\t" << endl;
+                std::cout << "  \t \t ------------------------------ \t" << endl;
 
-                if (player == 0)
+                if (player == 0 || checker_color == 0)
                 {
-                    std::cout << " \t \t   - Выберите режим игры" << std::endl;
-                }
-
-                if (checker_color == 0)
-                {
-                    std::cout << " \t \t   - Выберите цвет шашек" << std::endl;
+                    std::cout << "  \t \t ------------------------------ \t" << endl;
+                    std::cout << "  \t \t|      Выберите режим игры     |\t" << endl;
+                    std::cout << "  \t \t ------------------------------ \t" << endl;
+                    std::cout << "  \t \t ------------------------------ \t" << endl;
+                    std::cout << "  \t \t|      Выберите цвет шашек     |\t" << endl;
+                    std::cout << "  \t \t ------------------------------ \t" << endl;
                 }
             }
             else
             {
-
                 std::cout << " \t \t   Игра начинается!" << std::endl;
                 std::cout << " \t \t   Ваш выбор:" << std::endl;
-
 
                 if (player == 1 && checker_color == 1)
                 {
@@ -236,7 +168,100 @@ void menu()
 
                 std::cout << " \n \t \t   Удачи в игре!" << std::endl;
                 std::cin.get();
-                checkers_pole();
+            }
+            std::cout << " \n \t \t   Нажмите Enter для продолжения...";
+            std::cin.ignore();
+            std::cin.get();
+            system("cls");
+            break;
+
+        case 2:
+        {
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t|    ======== МЕНЮ ========    |\t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t|         Выбор игрока:        |\t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t|       1. Одиночная игра      |\t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t|       2. Игрок с игроком     |\t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t   Выбор игрока: ";
+            std::cin >> player;
+
+            system("cls");
+
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t|     Выберите цвет шашек:     |\t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t|         1. Белые шашки       |\t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t|         2. Черные шашки      |\t" << endl;
+            std::cout << "  \t \t ------------------------------ \t" << endl;
+            std::cout << "  \t \t   Выберите цвет шашек: ";
+            std::cin >> checker_color;
+            if (player == 1)
+            {
+                std::cout << "\n\t \t   Одиночная игра" << endl;
+            }
+            else if (player == 2)
+            {
+                std::cout << "\n\t \t   Игра на двоих" << endl;
+            }
+            else
+            {
+                std::cout << "\n  \t \t   Неверный выбор режима!" << endl;
+                player = 0;
+            }
+
+
+            if (checker_color == 1)
+            {
+                std::cout << " \n \t \t   Вы выбрали белые шашки" << endl;
+            }
+            else if (checker_color == 2)
+            {
+                std::cout << " \n \t \t   Вы выбрали черные шашки" << endl;
+            }
+            else
+            {
+                std::cout << " \n \t \t   Неверный выбор цвета!" << endl;
+                checker_color = 0;
+            }
+
+            std::cout << " \n \t \t   Нажмите Enter для возврата в меню...";
+            std::cin.ignore();
+            std::cin.get();
+            system("cls");
+            break;
+        }
+        case 3:
+            int a = 0;
+            std::cout << "  \t \t -------------------------------------\t" << endl;
+            std::cout << "  \t \t |                ВЫХОД              |\t" << endl;
+            std::cout << "  \t \t -------------------------------------\t" << endl;
+            std::cout << "  \t \t  ------------------------------------\t" << endl;
+            std::cout << "  \t \t |   Если вы хотите выйте нажмите 1  |\t" << endl;
+            std::cout << "  \t \t  ------------------------------------\t" << endl;
+            std::cout << "  \t \t  ------------------------------------\t" << endl;
+            std::cout << "  \t \t |Если вы хотите продолжить нажмите 2|\t" << endl;
+            std::cout << "  \t \t  ------------------------------------\t" << endl;
+            std::cout << "  \t \t   Ваш выбор: ";
+            cin >> a;
+            if (a == 1)
+            {
+                cout << "\n\t \t   Спосибо за выбор!" << endl;
+                return 0;
+            }
+            else if (a == 2)
+            {
+                cout << "\n\t \t   Спосибо за выбор!" << endl;
             }
             std::cout << " \n \t \t   Нажмите Enter для возврата в меню...";
             std::cin.ignore();
@@ -244,17 +269,6 @@ void menu()
             system("cls");
             break;
 
-        case 4:
-            cout << "\n \t ========\tВЫХОД \t ======== \n " << endl;
-            std::cout << "Выход из программы." << endl;
-            break;
-        default:
-            cout << " \n \t \t   Неверный выбор. Попробуйте снова." << endl;
-            cout << " \t \t   Нажмите Enter для продолжения...";
-            cin.ignore();
-            cin.get();
-            system("cls");
-            break;
         }
 
     }
